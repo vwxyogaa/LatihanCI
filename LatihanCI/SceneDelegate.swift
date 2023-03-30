9e560cdc361b18b244b399bee1eb003f438211a1
Panji Yoga <panjiyogap@gmail.com>
2023-03-30 17:08:22 +0700

    Initial Commit

---
+++ LatihanCI/SceneDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.swift
+//  LatihanCI
+//
+//  Created by Panji Yoga on 30/03/23.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ LatihanCI/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LatihanCI
+//
+//  Created by Panji Yoga on 30/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray
+    }
+}
